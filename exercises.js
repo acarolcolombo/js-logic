58ad292831ca3c74af6011fd8598b3c43f97426b
unknown <acarolcolombo@gmail.com>
2019-04-08 19:45:25 -1000

    exerc 2

--- exercises.js
+++ exercises.js
@@ -47,7 +47,13 @@ console.log(canVote(39));
  * The function will return the message: "Login Success!", if the string passed into the function is "test1234"
  * Console.log your result.
 */
-
+function login(password){
+  if (password === "test1234"){
+    return "Login Success!"
+  }
+  
+}
+console.log(login("test1234"));
 
 /*
  * #3
