d0c51210bdc49d7842c01a833af3c50cf18100c5
unknown <acarolcolombo@gmail.com>
2019-04-08 19:56:23 -1000

    exerc 5

--- exercises.js
+++ exercises.js
@@ -106,7 +106,13 @@ console.log(mustBeTrue(3 !== 2));
  * The function will return the message "Word to Big Bird!", if the string passed into the function is a three-letter word. 
  * Console.log your result.
 */
+function bigBird(word){
+  if (word.length === 3){
+    return "Word to Big Bird!"
+  }
+}
 
+console.log(bigBird("yes"));
 
 
 /*
